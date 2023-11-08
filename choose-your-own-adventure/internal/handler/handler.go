731f01d28ece706c19c40f6d216a159a3eacc20e
Alejandro Encarnación <alejandroencarnacion1@gmail.com>
2023-11-08 01:00:00 -0400

    Remove unnecessary interface from handler package

--- choose-your-own-adventure/internal/handler/handler.go
+++ choose-your-own-adventure/internal/handler/handler.go
@@ -8,24 +8,20 @@ import (
 	"github.com/aledeltoro/choose-your-own-adventure/internal/templating"
 )
 
-// Handler represents the interface to handle the adventure story rendering
-type Handler interface {
-	HandleRenderStory() http.HandlerFunc
-}
-
-type handler struct {
+// Handler processes requests to render adventure story
+type Handler struct {
 	service service.StoryService
 }
 
 // NewHandler creates a handler instance
 func NewHandler(service service.StoryService) Handler {
-	return handler{
+	return Handler{
 		service: service,
 	}
 }
 
 // HandleRenderStory renders the corresponding story page
-func (h handler) HandleRenderStory() http.HandlerFunc {
+func (h Handler) HandleRenderStory() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		currentStory := strings.ReplaceAll(r.URL.Path, "/cyoa/", "")
 
